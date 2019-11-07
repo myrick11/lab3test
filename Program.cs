44b0e8a9c035d9547a06db8c067f5ae19844c4ed
myrick11 <56942167+myrick11@users.noreply.github.com>
2019-11-06 19:45:11 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,83 @@
 ﻿using System;
 
-namespace Week1_Myrick
+namespace Inclassweek3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            bool isTrue;
 
-            int num1, num2, num3;
+            isTrue = false;
 
-            num1 = 27;
-            num2 = 55;
+            if (isTrue)
 
-            num3 = num1 * num2;
-            Console.WriteLine("The product of " + "num1" + " and " + num2 + " is " + num3);
+            {
+                Console.Write("Hello");
+                Console.Write("World!");
+            }
+
+            int num1 = 39;
+            if (num1 > 30)
+            {
+                Console.WriteLine("num1 is greater than 30");
+            }
+            else
+            {
+                if (num1 < 30)
+                {
+                    Console.WriteLine("num1 is less than 30");
+                }
+
+                else
+                {
+                    Console.WriteLine("num1 is equal to 30");
+                }
+                int grade = 75;
+
+                if (grade >= 90)
+                {
+                    Console.WriteLine("A");
+
+                }
+                else if (grade >= 80)
+                {
+                    Console.WriteLine("B");
+                }
+
+                else if (grade >= 70)
+                {
+                    Console.WriteLine("C");
+                }
+                else if (grade >= 60)
+                {
+                    Console.WriteLine("D");
+                }
+
+                else if (grade >= 0)
+                {
+                    Console.WriteLine("F");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Grade");
+                }
+
+
+
+
+
+                int counter = 10;
+                while (counter < 0)
+                {
+                    
+                   Console.WriteLine(counter);
+                   counter = counter - 2;
+                }
+
+
+            }
         }
     }
 }
+   
