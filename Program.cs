6f33a65bb883abcb9a88a8c02f6d1a01c1433623
myrick11 <56942167+myrick11@users.noreply.github.com>
2019-11-06 19:43:24 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Week1_Myrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1, num2, num3;
+
+            num1 = 27;
+            num2 = 55;
+
+            num3 = num1 * num2;
+            Console.WriteLine("The product of " + "num1" + " and " + num2 + " is " + num3);
+        }
+    }
+}
